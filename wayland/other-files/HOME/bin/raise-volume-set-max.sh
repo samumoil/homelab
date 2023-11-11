b5040e60ff9df29800f9a1da6c935840a12db64a
Sam <sam@controller.peikkoluola>
2023-11-11 22:24:44 +0200

    keybindings for volume and minor desktop settings for sway

---
+++ wayland/other-files/HOME/bin/raise-volume-set-max.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is modified from https://askubuntu.com/questions/1362023/set-max-limit-for-volume-increase-with-i3-pactl-and-bash
+
+# max_volume_pc is set in sway config file: 
+# It is last number of corresponding "bindsym" keybinding line
+
+#max_volume_pc=$1
+max_volume_pc=100
+current_volume_pc=$(pactl get-sink-volume @DEFAULT_SINK@ | grep 'Volume:' | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+if (($current_volume_pc < $max_volume_pc-4)) ; then
+    pactl set-sink-volume @DEFAULT_SINK@ +5%
+fi
